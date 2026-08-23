aacb8ac795a24032b48c6df0a4e61bf1fceb0d7a
traviscomber <n3uralia@gmail.com>
2026-08-22 20:45:34 -0400

    chore(ci): add ESLint 9 flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import nextPlugin from "@next/eslint-plugin-next"
+import tsParser from "@typescript-eslint/parser"
+
+export default [
+  {
+    ignores: [".next/**", "node_modules/**", "public/**"],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+  },
+]
