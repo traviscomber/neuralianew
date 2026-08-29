b2fddbc69eef4a657f265d2e1c532bd9241efb87
traviscomber <n3uralia@gmail.com>
2026-08-29 19:23:26 -0400

    style(recognition): load section 2 reference styles in Spanish

--- app/[locale]/reconocimiento/layout.tsx
+++ app/[locale]/reconocimiento/layout.tsx
@@ -1,4 +1,5 @@
 import '../recognition/hero-hud.css'
+import '../recognition-section2.css'
 
 export default function RecognitionLayout({ children }: { children: React.ReactNode }) {
   return children
