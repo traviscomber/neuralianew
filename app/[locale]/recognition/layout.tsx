588e133c8717847b57319a4eacbc289ceae20a18
traviscomber <n3uralia@gmail.com>
2026-08-29 22:10:22 -0400

    style(recognition): load section 3 value chain styles

--- app/[locale]/recognition/layout.tsx
+++ app/[locale]/recognition/layout.tsx
@@ -1,5 +1,6 @@
 import './hero-hud.css'
 import '../recognition-section2.css'
+import '../recognition-section3.css'
 
 export default function RecognitionLayout({ children }: { children: React.ReactNode }) {
   return children
