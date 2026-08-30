c60e913b52492a75dfb261efea0d4629dc883b86
traviscomber <n3uralia@gmail.com>
2026-08-30 11:46:35 -0400

    style(recognition): load section 04 PNG overrides in Spanish

--- app/[locale]/reconocimiento/layout.tsx
+++ app/[locale]/reconocimiento/layout.tsx
@@ -2,6 +2,7 @@ import '../recognition/hero-hud.css'
 import '../recognition-section2.css'
 import '../recognition-section3.css'
 import '../recognition-section4.css'
+import '../recognition-section4-png.css'
 
 export default function RecognitionLayout({ children }: { children: React.ReactNode }) {
   return children
