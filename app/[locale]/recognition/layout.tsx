983920e90873ae2d56648b801226b58f238b63b0
traviscomber <n3uralia@gmail.com>
2026-09-02 09:38:46 -0400

    refine(recognition): load Section 05 accessibility styles

--- app/[locale]/recognition/layout.tsx
+++ app/[locale]/recognition/layout.tsx
@@ -2,6 +2,7 @@ import './hero-hud.css'
 import '../recognition-section2.css'
 import '../recognition-section3.css'
 import '../recognition-section3-clean.css'
+import '../recognition-section05.css'
 
 export default function RecognitionLayout({ children }: { children: React.ReactNode }) {
   return children
