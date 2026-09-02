e10f23229d2c0c103c1aea3afa7362beb2341330
traviscomber <n3uralia@gmail.com>
2026-09-02 15:27:52 -0400

    fix(recognition): load typography normalization

--- app/[locale]/recognition/layout.tsx
+++ app/[locale]/recognition/layout.tsx
@@ -3,6 +3,7 @@ import '../recognition-section2.css'
 import '../recognition-section3.css'
 import '../recognition-section3-clean.css'
 import '../recognition-section05.css'
+import './typography-normalization.css'
 
 export default function RecognitionLayout({ children }: { children: React.ReactNode }) {
   return children
