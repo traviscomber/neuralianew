c3032bfa6484ecabf98f20e9b1248be548183013
traviscomber <n3uralia@gmail.com>
2026-09-08 18:13:53 -0300

    feat(solutions): enable rebuilt section 03

--- app/[locale]/soluciones/layout.tsx
+++ app/[locale]/soluciones/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react"
 import "./section02-redo.css"
+import "./section03-redo.css"
 
 export default function SolutionsLayout({ children }: { children: ReactNode }) {
   return children
