cb039d17e603e7995369e30e70955756b9879258
traviscomber <n3uralia@gmail.com>
2026-09-13 11:46:38 -0300

    feat: load Section 04 capabilities redesign

--- app/[locale]/solutions/layout.tsx
+++ app/[locale]/solutions/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react"
 import "../soluciones/section02-redo.css"
 import "../soluciones/section03.css"
+import "../soluciones/section04-redo.css"
 
 export default function SolutionsLayout({ children }: { children: ReactNode }) {
   return children
