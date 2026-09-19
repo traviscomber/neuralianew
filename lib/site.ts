f7b6e2b48a42ff8d476f224176d47adbdefb86cc
traviscomber <n3uralia@gmail.com>
2026-09-19 18:33:05 -0300

    remove unverified Twitter handle

--- lib/site.ts
+++ lib/site.ts
@@ -1,6 +1,5 @@
 export const SITE_NAME = "N3uralia"
 export const SITE_URL = "https://www.n3uralia.com"
-export const SITE_TWITTER_HANDLE = "@n3uralia"
 export const OG_IMAGE_PATH = "/og-image.png"
 
 export function absoluteUrl(path = "/") {
